848c6e947bf2953db058d6f02eb4ee81f6244e6c
1 <1@DESKTOP-NU9I6RS>
2023-09-27 23:08:31 +0800

    arrays in process

---
+++ DataTypes/Program.cs
@@ -0,0 +1,69 @@
+﻿//#define LOGICAL_TYPES
+//#define CHARACTER_TYPES
+//define INTEGRAL_TYPES
+#define FLOATIING_TYPES
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+	internal class Program
+	{
+		static readonly string delimiter = "\n----------------------------------------------------\n";
+		static void Main(string[] args)
+		{
+#if LOGICAL_TYPES
+			Console.WriteLine("BOOL: ");
+			Console.WriteLine(sizeof(bool));    // Boolean
+			Console.WriteLine(true.GetType());
+			Console.WriteLine((3 + 2.5).GetType());
+			Console.WriteLine(delimiter);
+#endif
+
+#if CHARACTER_TYPES
+			Console.WriteLine("CHAR: ");    //Хранит один символ в кодировке Unicode
+			Console.WriteLine(sizeof(char));
+			Console.WriteLine((int)(char.MinValue));
+			Console.WriteLine((int)char.MaxValue);
+			Console.WriteLine(delimiter);
+#endif
+
+#if INTEGRAL_TYPES
+			Console.WriteLine("SHORT: ");
+			Console.WriteLine(sizeof(ushort));
+			Console.WriteLine(ushort.MinValue);
+			Console.WriteLine(ushort.MaxValue);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine("INT: ");
+			Console.WriteLine(sizeof(int));
+			Console.WriteLine("UINT:\t" + uint.MinValue + " ... " + uint.MaxValue);
+			Console.WriteLine("INT:\t" + int.MinValue + " ... " + int.MaxValue);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine("LONG: ");
+			Console.WriteLine(sizeof(long));
+			Console.WriteLine("ULONG:\t" + ulong.MinValue + "... " + ulong.MaxValue);
+			Console.WriteLine("LONG:\t" + long.MinValue + "... " + long.MaxValue); 
+#endif
+
+#if FLOATIING_TYPES
+			Console.WriteLine("FLOAT:\t"+sizeof(float) + "bytes");
+			Console.WriteLine(float.MinValue + "..." + float.MaxValue);
+			Console.WriteLine(delimiter);
+			
+			Console.WriteLine("DOUBLE:\t" + sizeof(double) + "bytes");
+			Console.WriteLine(double.MinValue + "..." + double.MaxValue);
+			Console.WriteLine(delimiter);
+
+			Console.WriteLine("DECIMAL:\t" + sizeof(decimal) + "bytes");
+
+
+#endif
+
+		}
+	}
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,42 @@
+﻿//#define SHOOTER
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+	internal class Program
+	{
+		static readonly string delimiter = "\n----------------------------------------------------\n";
+		static void Main(string[] args)
+		{
+#if SHOOTER
+			int COMMAND;
+			Console.WriteLine("Нажмите 'W', 'A' 'S' 'D' для движения");
+			Console.WriteLine("Нажмите 'Space' для прыжка");
+			Console.WriteLine("Нажмите 'F' чтобы открыть огонь");
+			Console.WriteLine("Esc - выход");
+			Console.WriteLine(delimiter);
+			do
+			{
+				COMMAND = Console.ReadKey().KeyChar;
+				switch (COMMAND)
+				{
+					case 'w': case 'W': Console.WriteLine("Вперед"); break;
+					case 'a': case 'A': Console.WriteLine("Влево"); break;
+					case 's': case 'S': Console.WriteLine("Назад"); break;
+					case 'd': case 'D': Console.WriteLine("Вправо"); break;
+					case ' ': Console.WriteLine("Прыжок"); break;
+					case 'f': case 'F': Console.WriteLine("Огонь"); break;
+					default: Console.WriteLine("!Wrong command!"); break;
+				}
+
+			} while (COMMAND != 27); 
+#endif
+
+
+		}
+	}
+}
