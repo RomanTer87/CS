46525de3d00f59365fd444e0713844fc5b3fc347
1 <1@DESKTOP-NU9I6RS>
2023-09-29 22:23:58 +0800

    TransfersParameter

---
+++ TransfersParameters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransfersParameters
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int a;
+			int b;
+			Input(out a,out b);	
+			Console.WriteLine($"{a}\t{b}");
+			Exchange(ref a,ref b);
+			Console.WriteLine($"{a}\t{b}");
+		}
+		static void Input(out int a,out int b)
+		{
+			Console.Write("введите первое число"); a = Convert.ToInt32(Console.ReadLine());
+			Console.Write("введите второе число"); b = Convert.ToInt32(Console.ReadLine());
+
+		}
+	static void Exchange(ref int a, ref int b)
+		{
+			int buffer = a;
+			a = b;
+			b = buffer;
+		}
+	}
+}
